2595b9f92e00d7cfb8a61460fff54a0dc38cea24
jayel888 <15644@coderacademy.edu.au>
2024-06-06 20:01:49 +1000

    Practicing control flow

---
+++ sequential_flow.py
@@ -0,0 +1,10 @@
+age = 20
+has_permission = False
+
+if age >= 18:
+    if has_permission:
+        print("Access Granted.")
+    else:
+        print("Access Denied.")
+else:
+    print("Access Denied")
